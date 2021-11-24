a66212decfd829d4f201a1f80140c3c7b2a5f8dc
NonRatchanon <6250110009@psu.ac.th>
2021-11-25 03:11:21 +0700

    Add prettier

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-require("dotenv").config();
-const express = require("express");
-const app = express();
-const animeRouter = require("./routers/anime");
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const animeRouter = require('./routers/anime')
 
 //port
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //homepage route
-app.get("/", (req, res) => {
-  return res.send({
-    err: false,
-    msg: "Welcome to RESTful CRUD API with Nodejs, Express, Mysql",
-    written_by: "Ratchanon",
-  });
-});
+app.get('/', (req, res) => {
+    return res.send({
+        err: false,
+        msg: 'Welcome to RESTful CRUD API with Nodejs, Express, Mysql',
+        written_by: 'Ratchanon',
+    })
+})
 
 //Router
-app.use("/anime", animeRouter);
+app.use('/anime', animeRouter)
 
 // Run Server
 app.listen(port, () => {
-  console.log(`listening on port: ${port}...`);
-});
+    console.log(`listening on port: ${port}...`)
+})
--- routers/anime.js
+++ routers/anime.js
@@ -1,99 +1,104 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 //prisma
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+const { PrismaClient } = require('@prisma/client')
+const prisma = new PrismaClient()
 
 //GET read
-router.get("/", async (req, res) => {
-  const animes = await prisma.anime.findMany();
+router.get('/', async (req, res) => {
+    const animes = await prisma.anime.findMany()
 
-  if (animes.length == 0) {
-    msg = "Anime table is empty";
-  } else {
-    msg = "Successfully retrieved all Anime";
-  }
+    if (animes.length == 0) {
+        msg = 'Anime table is empty'
+    } else {
+        msg = 'Successfully retrieved all Anime'
+    }
 
-  res.send({
-    err: false,
-    data: animes,
-    msg: msg,
-  });
-});
+    res.send({
+        err: false,
+        data: animes,
+        msg: msg,
+    })
+})
 
 //GET by id || read 1
 
-router.get("/:id", async (req, res) => {
-  const id = Number(req.params.id);
-  const anime = await prisma.anime.findUnique({
-    where: {
-      id: id,
-    },
-  });
+router.get('/:id', async (req, res) => {
+    const id = Number(req.params.id)
+    const anime = await prisma.anime.findUnique({
+        where: {
+            id: id,
+        },
+    })
 
-  if (anime === null) {
-    msg = "Anime table is empty";
-  } else {
-    msg = "Successfully retrieved Anime data";
-  }
+    if (anime === null) {
+        msg = 'Anime table is empty'
+    } else {
+        msg = 'Successfully retrieved Anime data'
+    }
 
-  res.send({
-    err: false,
-    data: anime,
-    msg: msg,
-  });
-});
+    res.send({
+        err: false,
+        data: anime,
+        msg: msg,
+    })
+})
 
 //POST Insert
-router.post("/", async (req, res) => {
-  const { name, author } = req.body;
+router.post('/', async (req, res) => {
+    const { name, author } = req.body
 
-  //validation
-  if (!name || !author) {
-    return res
-      .status(400)
-      .send({ err: true, msg: "Please provide anime name and author" });
-  }
-  const anime = await prisma.anime.create({
-    data: {
-      name: name,
-      author: author,
-    },
-  });
-  res.send({ err: false, data: anime, msg: "Anime Successfully added" });
-});
+    //validation
+    if (!name || !author) {
+        return res
+            .status(400)
+            .send({ err: true, msg: 'Please provide anime name and author' })
+    }
+    const anime = await prisma.anime.create({
+        data: {
+            name: name,
+            author: author,
+        },
+    })
+    res.send({ err: false, data: anime, msg: 'Anime Successfully added' })
+})
 
 //Put Update
-router.put("/:id", async (req, res) => {
-  const id = Number(req.params.id);
-  const { name, author } = req.body;
+router.put('/:id', async (req, res) => {
+    const id = Number(req.params.id)
+    const { name, author } = req.body
 
-  //validation
-  if (!id || !name || !author) {
-    return res
-      .status(400)
-      .send({ err: true, msg: "Please provide anime id, name and author" });
-  }
-  const anime = await prisma.anime.update({
-    where: { id: id },
-    data: { name: name, author: author },
-  });
-  res.send({ err: false, data: anime, msg: "Anime Successfully Updated" });
-});
+    //validation
+    if (!id || !name || !author) {
+        return res
+            .status(400)
+            .send({
+                err: true,
+                msg: 'Please provide anime id, name and author',
+            })
+    }
+    const anime = await prisma.anime.update({
+        where: { id: id },
+        data: { name: name, author: author },
+    })
+    res.send({ err: false, data: anime, msg: 'Anime Successfully Updated' })
+})
 
 //Delete by id
-router.delete("/:id", async (req, res) => {
-  const id = Number(req.params.id);
-  if (!id) {
-    return res.status(400).send({ err: true, msg: "Please provide anime id" });
-  }
+router.delete('/:id', async (req, res) => {
+    const id = Number(req.params.id)
+    if (!id) {
+        return res
+            .status(400)
+            .send({ err: true, msg: 'Please provide anime id' })
+    }
 
-  const anime = await prisma.anime.delete({
-    where: { id: id },
-  });
+    const anime = await prisma.anime.delete({
+        where: { id: id },
+    })
 
-  res.status(204).end();
-});
+    res.status(204).end()
+})
 
-module.exports = router;
+module.exports = router
